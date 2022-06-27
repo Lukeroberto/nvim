4ba75ade46de7a4eba6d57f86a19a75af20a08f6
luke <lukeroberto213@gmail.com>
2022-06-27 17:41:36 -0400

    Added setup file + update init.lua to prevent breakage on new system

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-vim.opt.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
-vim.cmd([[set tabstop=4]])
-
 require('plugins')
 require('lualine').setup({options = {theme = 'gruvbox'}})
 require('lspconfig').pyright.setup{}
+
+vim.opt.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[set tabstop=4]])
 
