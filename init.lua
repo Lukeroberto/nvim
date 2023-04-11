ce17610eacc5c104a46ab595fa4f5f0a1104b522
luke roberto <lukeroberto213@gmail.com>
2023-04-10 21:29:31 -0400

    add lsp config

--- init.lua
+++ init.lua
@@ -29,20 +29,39 @@ vim.cmd([[colorscheme gruvbox]])
 --Package management
 return require('packer').startup(function(use)
 	use {'wbthomason/packer.nvim'}
-	
 	use {'ellisonleao/gruvbox.nvim'}
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
 	}
 	use {'nvim-treesitter/nvim-treesitter-context'}
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		requres = { {'nvim-lua/plenary.nvim'} }
-	}
+	use {'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = {
+            { 'nvim-lua/plenary.nvim' }
+        }
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocomplete
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 
 	if packer_bootstrap then 
 		require('packer').sync()
 	end
 end)
-	
